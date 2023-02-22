85d37f206020983a47efbdfd7bf8975d408b8faa
Adaeze Chukwukereze <adaeze633@gmail.com>
2023-02-22 12:45:11 +0100

    new

--- CNN/settings.py
+++ CNN/settings.py
@@ -24,7 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+# SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = 'django-insecure-l8y6&)ho_z8$!xb-7@yk)^=!-tv@c8x7pw-0acg=!qm9%e8faz'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
